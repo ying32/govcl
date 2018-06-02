0aeef2b9ae19c5e0f2d3842ba0888381a18d6b40
ying32 <ying_33@live.cn>
2018-06-03 04:02:22 +0800

    更新govcl例程，测试新添加的方法

--- main.go
+++ main.go
@@ -31,6 +31,10 @@ func main() {
 		}
 	}()
 
+	fmt.Println("InheritsFrom：", vcl.Application.InheritsFrom(vcl.TObjectClass()))
+	fmt.Println("InheritsFrom：", vcl.Application.InheritsFrom(vcl.TComponentClass()))
+	fmt.Println("InheritsFrom：", vcl.Application.InheritsFrom(vcl.TControlClass()))
+
 	fmt.Println("main")
 	vcl.Application.SetIconResId(3)
 	vcl.Application.Initialize()
