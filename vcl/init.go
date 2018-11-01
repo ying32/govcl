03a06a2792fc3c0c390db0450713378d0cef3ac0
ying32 <ying_33@live.cn>
2018-11-01 11:13:19 +0800

    fix commnet

--- vcl/init.go
+++ vcl/init.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	// 四个实例类，不需要Create和Free即可访问
+	// 几个实例类，不需要Create和Free即可访问
 	Application *TApplication
 	Screen      *TScreen
 	Mouse       *TMouse
@@ -26,7 +26,7 @@ func init() {
 	// 设置事件的回调函数，因go中callback数量有限，只好折中处理
 	SetEventCallback(callbackStdcall)
 
-	// 导入四个实例类
+	// 导入几个实例类
 	Application = ApplicationFromInst(Application_Instance())
 	Screen = ScreenFromInst(Screen_Instance())
 	Mouse = MouseFromInst(Mouse_Instance())
