7ba55adb512ac5f57a0966bac4a544b8b8b2a5f1
ying32 <ying_33@live.cn>
2018-11-08 04:12:04 +0800

    Update rtl

--- vcl/rtl/init_posix.go
+++ vcl/rtl/init_posix.go
@@ -1,3 +1,5 @@
+// +build !windows
+
 package rtl
 
 import (
@@ -9,6 +11,8 @@ import (
 func init() {
 	SysLocale.FarEast = true
 	SysLocale.MiddleEast = true
+	// 这个环境变量在MacOS下只有通过命令行启动的才能获取得到。
+	// Linux下不知道有哪些不能获取到的。
 	lang := os.Getenv("LANG")
 	if lang != "" {
 		lang = strings.Replace(lang, "_", "-", -1)
