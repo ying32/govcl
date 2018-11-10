400555c372147c819295d0c8268ab4e45bbecb44
ying32 <ying_33@live.cn>
2018-11-10 16:24:05 +0800

    1. The event type is automatically parsed in the "Associated Event". 2. Add a tag field for the res2go shared event.
    1、“关联事件”中自动解析事件类型。
    2、增加tag字段用于res2go共享事件。

--- vcl/associatedEvents.go
+++ vcl/associatedEvents.go
@@ -2,11 +2,31 @@
    这里主要是窗口资源用来反射关联事件，就可以简化相关代码。
 
    事件名命名的规则：
-     On + 组件名 + 事件，TForm特殊性，固定为名称为Form， 相关支持的事件见： _events中
+     On + 组件名 + 事件，TForm特殊性，固定为名称为Form。
    例如窗口建完后：
      func (f *TForm1) OnFormCreate(sender vcl.IObject)
    又如按钮：
      func (f *TForm1) OnButton1Click(sender vcl.IObject)
+
+   原理：
+     首次会收集Form以On开头的事件，然后根据 组件名称提取出事件的类型，再通过事件类型查找某个组件中的 SetOn + eventType方法。
+
+   多个组件共享同一个事件：
+
+   type TMainForm struct {
+       *vcl.TForm
+       Button1 *vcl.TButton
+       Button2 *vcl.TButton `event:"OnButton1Click"`
+       Button3 *vcl.TButton `event:"OnButton1Click"`
+   }
+
+   // 这样只自动关联了Button1的事件，但此时我想将此事件关联到Button2, Button3上
+   // 常规的做法就是 Button2.SetOnClick(f.OnButton1Click)
+   // 现在提供一种新的方式，这种方式应对于res2go转换后不自动共享事件问题。
+
+   func (f *TMainForm) OnButton1Click(sender vcl.IObject) {
+
+   }
 
 */
 
@@ -18,204 +38,136 @@ import (
 	"strings"
 )
 
-var (
-
-	// 这里的事件还不全，以后继续补库，先满足基本需求
-	_events = []string{
-		"Click",
-		"Update",
-		"Execute",
-		"MouseDown",
-		"MouseUp",
-		"MouseMove",
-		"MouseWheel",
-		"MouseEnter",
-		"MouseLeave",
-		"CloseQuery",
-		"Destroy",
-		"Show",
-		"Hide",
-		"Close",
-		"Find",
-		"Replace",
-		"LinkClick",
-		"Change",
-		"KeyPress",
-		"DblClick",
-		"Resize",
-		"KeyDown",
-		"KeyUp",
-		"DropFiles",
-		"Activate",
-		"Deactivate",
-		"ConstrainedResize",
-		"Paint",
-		"ContextPopup",
-		"DragOver",
-		"DragDrop",
-		"StartDrag",
-		"EndDrag",
-		"DockDrop",
-		"DockOver",
-		"UnDock",
-		"StartDock",
-		"GetSiteInfo",
-		"MouseWheelDown",
-		"MouseWheelUp",
-		"Timer",
-		"StyleChanged",
-		// Grid
-		"ColumnMoved",
-		"DrawCell",
-		"FixedCellClick", // libvcl
-		"GetEditMask",
-		"GetEditText",
-		"RowMoved",
-		"SelectCell",
-		"SetEditText",
-		"TopLeftChanged",
-
-		//headercontrol
-		"DrawSection",  // livcl
-		"SectionCheck", // libvcl
-		"SectionClick", // libvcl
-		"SectionDrag",
-		"SectionEndDrag",
-		"SectionResize",
-		"SectionTrack"}
-
-	// Application 独有事件
-	_appEvents = []string{
-		"Hint",
-		"Exception",
-		"Minimize",
-		"Restore",
-		"Message",
-		"Help",
-		"ShortCut"}
-)
-
-// RegisterFormEvent 这个函数主要是我没有在这里添加时，可直接注册相关事件
-func RegisterFormEvent(eventType string) {
-	for _, tpy := range _events {
-		if strings.Compare(strings.ToLower(tpy), strings.ToLower(eventType)) == 0 {
-			return
-		}
-	}
-	_events = append(_events, eventType)
-}
-
-func iifstr(b bool, atrue, afalse string) string {
-	if b {
-		return atrue
-	}
-	return afalse
-}
-
 // associatedEvents 关联事件。
 func associatedEvents(vForm reflect.Value, form *TForm, subComponentEvent bool) {
 	defer func() {
 		if err := recover(); err != nil {
-			fmt.Println("associatedEvents ERROR: ", err)
+			fmt.Println("associatedEvents error: ", err)
 		}
 	}()
-	// test test test
-	// 关联事件, 查找组件
+	// OnFormCreate
+	var formCreate reflect.Value
+
 	vt := vForm.Type()
+
+	// 提取所有符合规则的事件
 	eventMethods := make(map[string]reflect.Value, 0)
 	for i := 0; i < vt.NumMethod(); i++ {
 		m := vt.Method(i)
+		// 保存窗口创建事件
+		if m.Name == "OnFormCreate" {
+			formCreate = vForm.Method(i)
+			continue
+		}
 		if strings.HasPrefix(m.Name, "On") {
 			eventMethods[m.Name] = vForm.Method(i)
 		}
 	}
-	// 简化查找
-	findMethod := func(name, event string) (reflect.Value, bool) {
-		v, ok := eventMethods[fmt.Sprintf("On%s%s", name, event)]
-		return v, ok
+
+	type tempItem struct {
+		Type   string
+		Method reflect.Value
 	}
 
-	setEvent := func(component IComponent, isForm bool) {
-		name := iifstr(isForm, "TForm", component.Name())
-		name2 := iifstr(isForm, "Form", name)
-		if name == "" {
-			return
+	// 临时方法表
+	tempEventTypes := make(map[string]tempItem, 0)
+
+	// 设置事件
+	setEvent := func(component IComponent, name1, name2 string) {
+		if name1 == "" {
+			name1 = component.Name()
 		}
-		for _, eventType := range _events {
-			// 遍沥组件，找寻方法
-			if method, ok := findMethod(name2, eventType); ok {
-				// 因为TForm的特殊性，所以需要重新命名下
-				addNotifyEvent(vForm, name, method, component, eventType)
+		if name2 == "" {
+			name2 = name1
+		}
+		prefix := "On" + name1
+		for mName, method := range eventMethods {
+			if !strings.HasPrefix(mName, prefix) {
+				continue
+			}
+			eventType := mName[len(prefix):]
+			// 将事件名与事件类型对应，之后会用到的
+			tempEventTypes[mName] = tempItem{eventType, method}
+
+			if component.Equals(Application) {
+				addApplicationNotifyEvent(eventType, method)
+			} else {
+				addComponentNotifyEvent(vForm, name2, method, eventType)
 			}
 		}
 	}
 
-	setEvent(form, true)
+	// 设置Form事件
+	setEvent(form, "Form", "TForm")
+
+	// 设置子组件事件
 	if subComponentEvent {
 		var i int32
 		for i = 0; i < form.ComponentCount(); i++ {
-			setEvent(form.Components(i), false)
+			setEvent(form.Components(i), "", "")
+		}
+
+		// 提取字段中的事件关联
+		for i := 0; i < vt.Elem().NumField(); i++ {
+			field := vt.Elem().Field(i)
+			eventTag := field.Tag.Get("event")
+			if eventTag == "" {
+				continue
+			}
+			item, ok := tempEventTypes[eventTag]
+			if !ok {
+				continue
+			}
+			if vCtl := vForm.Elem().Field(i); vCtl.IsValid() {
+				findAndSetEvent(vCtl, item.Type, item.Method)
+			}
 		}
 	}
 
-	// 一些特殊的，比如Application
-	addApplicationNotifyEvent(vForm, eventMethods)
+	// 设置Application事件
+	setEvent(Application, "Application", "")
+
 	// 最后调用OnCreate
-	callFormCreate(vForm)
+	callEvent(formCreate, []reflect.Value{vForm})
 }
 
-func callFormCreate(vv reflect.Value) {
+// callEvent 调用事件。
+func callEvent(event reflect.Value, params []reflect.Value) {
 	defer func() {
 		if err := recover(); err != nil {
-			fmt.Println("callFormCreate ERROR: ", err)
+			fmt.Println("callEvent error:", err)
 		}
 	}()
-	// 查找默认的Form创建，命名规则以 On+组件名+方法名
-	m := vv.MethodByName("OnFormCreate")
-	if m.IsValid() {
-		m.Call([]reflect.Value{vv})
+	if !event.IsValid() {
+		return
 	}
+	event.Call(params)
 }
 
-// callSetEventMethod 公用的call SetOnXXXX方法
-func callSetEventMethod(v reflect.Value, eventType string, component IObject, method reflect.Value) {
+// findAndSetEvent 公用的call SetOnXXXX方法
+func findAndSetEvent(v reflect.Value, eventType string, method reflect.Value) {
 	defer func() {
 		if err := recover(); err != nil {
-			fmt.Println("callSetEventMethod ERROR: ", err, ", eventType:", eventType, ", Component:", component)
+			fmt.Println("findAndSetEvent error: ", err, ", eventType:", eventType)
 		}
 	}()
-	event := v.MethodByName("SetOn" + eventType)
-	if event.IsValid() {
-		//fnx := reflect.MakeFunc(method.Type(), func(args []reflect.Value) (results []reflect.Value) {
-		//	return method.Call(args)
-		//})
+	if event := v.MethodByName("SetOn" + eventType); event.IsValid() {
 		event.Call([]reflect.Value{method})
 	}
 }
 
-// addNotifyEvent
-func addNotifyEvent(vv reflect.Value, compName string, method reflect.Value, component IObject, eventType string) {
-	vCtl := vv.Elem().FieldByName(compName)
-	if !vCtl.IsValid() {
-		return
+// addComponentNotifyEvent
+func addComponentNotifyEvent(vForm reflect.Value, compName string, method reflect.Value, eventType string) {
+	if vCtl := vForm.Elem().FieldByName(compName); vCtl.IsValid() {
+		findAndSetEvent(vCtl, eventType, method)
 	}
-	callSetEventMethod(vCtl, eventType, component, method)
 }
 
 // addApplicationNotifyEvent
 // 添加Application的关联事件，在一个程序内，application只的事件只有最后一次设置的才会生效。
 // 因为Application是单例存在，推荐在主窗口内处理就行了。
-func addApplicationNotifyEvent(vv reflect.Value, eventMethods map[string]reflect.Value) {
-	app := reflect.ValueOf(Application)
-	if app.IsValid() {
-		findMethod := func(event string) (reflect.Value, bool) {
-			v, ok := eventMethods[fmt.Sprintf("OnApplication%s", event)]
-			return v, ok
-		}
-		for _, eventType := range _appEvents {
-			if m, ok := findMethod(eventType); ok {
-				callSetEventMethod(app, eventType, Application, m)
-			}
-		}
+func addApplicationNotifyEvent(eventType string, method reflect.Value) {
+	if app := reflect.ValueOf(Application); app.IsValid() {
+		findAndSetEvent(app, eventType, method)
 	}
 }
