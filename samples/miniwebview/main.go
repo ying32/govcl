1ff9b6b7f5647ba192ffd64b34984efdc3543521
ying32 <ying_33@live.cn>
2018-11-13 09:46:12 +0800

    Update the miniwebview example.

--- samples/miniwebview/main.go
+++ samples/miniwebview/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/ying32/govcl/vcl"
+	"github.com/ying32/govcl/vcl/exts/tools"
 )
 
 func main() {
+	tools.RunWithMacOSApp()
 	vcl.Application.Initialize()
 	vcl.Application.CreateForm(mainFormBytes, &MainForm)
 	vcl.Application.Run()
