52a172187f4ed959b7a4c1679cbe8ae1a6be092e
ying32 <ying_33@live.cn>
2018-11-16 08:02:05 +0800

    Update the mp3Player example.

--- samples/mp3Player/Form1Impl.go
+++ samples/mp3Player/Form1Impl.go
@@ -9,48 +9,58 @@ import (
 	"github.com/ying32/govcl/vcl"
 )
 
-var (
+//::private::
+type TForm1Fields struct {
 	bassPlayer *bass.TBass
-)
+}
 
 func (f *TForm1) OnFormCreate(sender vcl.IObject) {
-	bassPlayer = bass.NewBass()
+	f.bassPlayer = bass.NewBass()
 	f.Timer1.SetEnabled(true)
 }
 
 func (f *TForm1) OnFormDestroy(sender vcl.IObject) {
-	bassPlayer.Close()
+	f.bassPlayer.Close()
 	bass.BassFree()
 }
 
 func (f *TForm1) OnBtnOpenClick(sender vcl.IObject) {
 	if f.OpenDialog1.Execute() {
-		fmt.Println(bassPlayer.OpenFile(f.OpenDialog1.FileName()))
-		mLen, err := bassPlayer.GetLength()
+		fmt.Println(f.bassPlayer.OpenFile(f.OpenDialog1.FileName()))
+		mLen, err := f.bassPlayer.GetLength()
 		fmt.Println("length:", mLen, ", err:", err)
 		f.TbPostion.SetMax(int32(mLen))
+		f.bassPlayer.SetVolume(int(f.TbVol.Position()))
 	}
 }
 
 func (f *TForm1) OnBtnPlayClick(sender vcl.IObject) {
-	fmt.Println(bassPlayer.Play(false))
+	fmt.Println(f.bassPlayer.Play(false))
 }
 
 func (f *TForm1) OnTimer1Timer(sender vcl.IObject) {
-	if bassPlayer.IsValid() && bassPlayer.State == bass.PsPlaying {
-		f.LblTime.SetCaption(bassPlayer.TimeStrLabel())
-		pos, _ := bassPlayer.GetPosition()
+	if f.bassPlayer.IsValid() && f.bassPlayer.State == bass.PsPlaying {
+		f.LblTime.SetCaption(f.bassPlayer.TimeStrLabel())
+		pos, _ := f.bassPlayer.GetPosition()
 		f.TbPostion.SetPosition(int32(pos))
-
 	}
 }
 
 func (f *TForm1) OnTbVolChange(sender vcl.IObject) {
-	if bassPlayer.IsValid() {
-		bassPlayer.SetVolume(int(f.TbVol.Position()))
-	}
+	f.bassPlayer.SetVolume(int(f.TbVol.Position()))
 }
 
 func (f *TForm1) OnTbPostionChange(sender vcl.IObject) {
 
+}
+
+func (f *TForm1) OnBtnPauseClick(sender vcl.IObject) {
+	f.bassPlayer.Pause()
+}
+
+func (f *TForm1) OnBtnStopClick(sender vcl.IObject) {
+	f.bassPlayer.Stop()
+	f.Timer1.SetEnabled(false)
+	f.bassPlayer.SetPosition(0)
+	f.TbPostion.SetPosition(0)
 }
