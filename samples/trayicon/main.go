096ea4d926a9ba25abeec2ab7e7a50ef086a9bab
ying32 <ying_33@live.cn>
2018-11-27 21:02:01 +0800

    Update the trayicon example.

--- samples/trayicon/main.go
+++ samples/trayicon/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/ying32/govcl/vcl"
 
+	"github.com/ying32/govcl/vcl/exts/tools"
 	"github.com/ying32/govcl/vcl/rtl"
 	"github.com/ying32/govcl/vcl/types"
 )
 
 func main() {
+	tools.RunWithMacOSApp()
 	vcl.Application.SetIconResId(3)
 	vcl.Application.Initialize()
 	vcl.Application.SetMainFormOnTaskBar(true)
@@ -17,36 +20,54 @@ func main() {
 	mainForm := vcl.Application.CreateForm()
 	mainForm.SetCaption("Hello")
 	mainForm.SetPosition(types.PoScreenCenter)
+
+	trayicon := vcl.NewTrayIcon(mainForm)
+
+	btn := vcl.NewButton(mainForm)
+	btn.SetParent(mainForm)
+	btn.SetCaption("ShowBalloon")
+	btn.SetLeft(20)
+	btn.SetTop(30)
+	btn.SetWidth(100)
+	btn.SetOnClick(func(vcl.IObject) {
+		trayicon.SetBalloonTitle("test")
+		trayicon.SetBalloonTimeout(1000)
+		trayicon.SetBalloonHint("我是提示正文啦")
+		trayicon.ShowBalloonHint()
+	})
+
 	pm := vcl.NewPopupMenu(mainForm)
+
 	item := vcl.NewMenuItem(mainForm)
 	item.SetCaption("退出(&E)")
 	item.SetOnClick(func(vcl.IObject) {
 		mainForm.Close()
 	})
 	pm.Items().Add(item)
-
-	trayicon := vcl.NewTrayIcon(mainForm)
-	//trayicon.SetIcon(icon) 不设置则使用Application.Icon现有的
+	trayicon.SetPopupMenu(pm)
 	// lcl库得手指定
 	if rtl.LcLLoaded() {
-		trayicon.SetIcon(vcl.Application.Icon())
+		if runtime.GOOS == "darwin" {
+			icon := vcl.NewIcon()
+			icon.LoadFromFile(rtl.ExtractFilePath(vcl.Application.ExeName()) + "/2.ico")
+			trayicon.SetIcon(icon)
+			icon.Free()
+		} else {
+			trayicon.SetIcon(vcl.Application.Icon())
+		}
 	}
 	trayicon.SetHint(mainForm.Caption())
 	trayicon.SetVisible(true)
-	trayicon.SetOnDblClick(func(vcl.IObject) {
-		trayicon.SetBalloonTitle("test")
-		trayicon.SetBalloonTimeout(10000)
-		trayicon.SetBalloonHint("我是提示正文啦")
-		trayicon.ShowBalloonHint()
-		fmt.Println("TrayIcon Click.")
-	})
-	trayicon.SetPopupMenu(pm)
-	// 其它事件请看源代码中以 SetOn 开头的
-	//	trayicon.SetOnMouseDown(func(Sender vcl.IObject, Button, Shift, X, Y int32) {
-	//		if Button == types.MbRight {
-	//			vcl.ShowMessage("fff")
-	//		}
-	//	})
 
+	if runtime.GOOS == "windows" {
+		trayicon.SetOnDblClick(func(vcl.IObject) {
+			// macOS似乎不支持双击
+			trayicon.SetBalloonTitle("test")
+			trayicon.SetBalloonTimeout(10000)
+			trayicon.SetBalloonHint("我是提示正文啦")
+			trayicon.ShowBalloonHint()
+			fmt.Println("TrayIcon DClick.")
+		})
+	}
 	vcl.Application.Run()
 }
