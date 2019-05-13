ccf6c4c7002a5ecb2dc2aa1627733fd427ce17b1
ying32 <ying_33@live.cn>
2019-05-13 19:43:56 +0800

    Adjust the PageControlWizard example
    调整PageControlWizard例子

--- samples/pagecontrolwizard/MainFormImpl.go
+++ samples/pagecontrolwizard/MainFormImpl.go
@@ -12,18 +12,39 @@ type TMainFormFields struct {
 
 func (f *TMainForm) OnFormCreate(sender vcl.IObject) {
 	f.hideAllTab()
-	f.PageControl1.SetActivePageIndex(0)
+	f.setPage(0)
 }
 
 func (f *TMainForm) hideAllTab() {
 	var i int32
 	for i = 0; i < f.PageControl1.PageCount(); i++ {
-		f.PageControl1.Pages(i).SetTabVisible(false)
+		sheet := f.PageControl1.Pages(i)
+		sheet.SetTabVisible(false)
+		sheet.SetVisible(false)
 	}
 }
 
+func (f *TMainForm) setPage(idx int32) {
+	if idx != 0 && idx != -1 && idx != 1 {
+		return
+	}
+	if idx == 0 {
+		f.PageControl1.SetActivePageIndex(0)
+		sheet := f.PageControl1.Pages(0)
+		sheet.SetVisible(true)
+		return
+	}
+	curIdx := f.PageControl1.ActivePageIndex()
+	sheet := f.PageControl1.Pages(curIdx)
+	sheet.SetVisible(false)
+	f.PageControl1.SetActivePageIndex(curIdx + idx)
+	sheet = f.PageControl1.Pages(curIdx + idx)
+	sheet.SetVisible(true)
+}
+
 func (f *TMainForm) OnActPagePrevExecute(sender vcl.IObject) {
-	f.PageControl1.SetActivePageIndex(f.PageControl1.ActivePageIndex() - 1)
+	//f.PageControl1.SetActivePageIndex(f.PageControl1.ActivePageIndex() - 1)
+	f.setPage(-1)
 }
 
 func (f *TMainForm) OnActPagePrevUpdate(sender vcl.IObject) {
@@ -31,7 +52,8 @@ func (f *TMainForm) OnActPagePrevUpdate(sender vcl.IObject) {
 }
 
 func (f *TMainForm) OnActPageNextExecute(sender vcl.IObject) {
-	f.PageControl1.SetActivePageIndex(f.PageControl1.ActivePageIndex() + 1)
+	//f.PageControl1.SetActivePageIndex(f.PageControl1.ActivePageIndex() + 1)
+	f.setPage(1)
 }
 
 func (f *TMainForm) OnActPageNextUpdate(sender vcl.IObject) {
