dadb241f7e9bab112842fe163c45fbe46486020f
ying32 <ying_33@live.cn>
2019-06-05 23:43:30 +0800

    Update

--- samples/miniblinkWebview/MainFormImpl.go
+++ samples/miniblinkWebview/MainFormImpl.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/ying32/govcl/vcl/types/keys"
+	"github.com/ying32/govcl/vcl/win"
+
 	"github.com/ying32/govcl/vcl/types"
 
 	"github.com/ying32/govcl/vcl"
@@ -19,6 +22,7 @@ type TMainFormFields struct {
 
 func (f *TMainForm) OnFormCreate(sender vcl.IObject) {
 	f.ScreenCenter()
+	fmt.Println("OnFormCreate:", win.GetCurrentThreadId())
 	f.web = miniblink.NewMiniBlinkWebview(f.PnlWeb.Handle())
 	f.web.Show(true)
 	f.web.OnTitleChanged = f.onWkeTitleChanged
@@ -37,21 +41,13 @@ func (f *TMainForm) OnPnlWebResize(sender vcl.IObject) {
 }
 
 func (f *TMainForm) onWkeTitleChanged(sender *miniblink.TMiniBlinkWebview, title string) {
-	fmt.Println("标题改变")
+	fmt.Println("标题改变:", title)
+	fmt.Println("onTitleChanged:", win.GetCurrentThreadId())
 	f.SetCaption(title + " - ying32")
 }
 
 func (f *TMainForm) OnBtnReloadClick(sender vcl.IObject) {
 	f.web.Webview.Reload()
-}
-
-func (f *TMainForm) OnBtnNavClick(sender vcl.IObject) {
-	text := strings.TrimSpace(f.EdtURL.Text())
-	if text == "" {
-		vcl.ShowMessage("请输入一个URL！")
-		return
-	}
-	f.web.Webview.LoadURL(text)
 }
 
 func (f *TMainForm) OnBtnLoadFromFileClick(sender vcl.IObject) {
@@ -61,17 +57,79 @@ func (f *TMainForm) OnBtnLoadFromFileClick(sender vcl.IObject) {
 	}
 }
 
-func (f *TMainForm) OnBtnBackClick(sender vcl.IObject) {
-	f.web.Webview.GoBack()
-}
-
-func (f *TMainForm) OnBtnForwardClick(sender vcl.IObject) {
-	f.web.Webview.GoForward()
-}
-
 func (f *TMainForm) OnBtnLoadFromStringClick(sender vcl.IObject) {
 	result := HTMLForm.ShowModal()
 	if result == types.MrOk {
 		f.web.Webview.LoadHTML(HTMLForm.Memo1.Text())
+	}
+}
+
+//func (f *TMainForm) OnFormKeyPress(sender vcl.IObject, key *types.Char) {
+//
+//}
+
+func (f *TMainForm) OnActGoBackExecute(sender vcl.IObject) {
+	f.web.Webview.GoBack()
+}
+
+func (f *TMainForm) OnActGoBackUpdate(sender vcl.IObject) {
+	vcl.ActionFromObj(sender).SetEnabled(f.web != nil && f.web.IsValid() && f.web.Webview.CanGoBack())
+}
+
+func (f *TMainForm) OnActGoForwardExecute(sender vcl.IObject) {
+	f.web.Webview.GoForward()
+}
+
+func (f *TMainForm) OnActGoForwardUpdate(sender vcl.IObject) {
+	vcl.ActionFromObj(sender).SetEnabled(f.web != nil && f.web.IsValid() && f.web.Webview.CanGoForward())
+}
+
+func (f *TMainForm) OnActNavExecute(sender vcl.IObject) {
+	text := strings.TrimSpace(f.EdtURL.Text())
+	if text == "" {
+		vcl.ShowMessage("请输入一个URL！")
+		return
+	}
+	f.web.Webview.LoadURL(text)
+}
+
+func (f *TMainForm) OnActNavUpdate(sender vcl.IObject) {
+	//vcl.ActionFromObj(sender).SetEnabled(f.web != nil && f.web.IsValid() && f.web.Webview.CanGoForward())
+}
+
+func (f *TMainForm) OnFormKeyDown(sender vcl.IObject, key *types.Char, shift types.TShiftState) {
+	if *key == keys.VkReturn {
+		f.ActNav.Execute()
+		*key = 0
+	}
+}
+
+func (f *TMainForm) OnBtnExecJSClick(sender vcl.IObject) {
+	result := JsForm.ShowModal()
+	if result == types.MrOk && f.web.IsValid() {
+		val := f.web.Webview.RunJS(JsForm.Memo1.Text())
+		switch val.TypeOf() {
+		case miniblink.JSTYPE_NUMBER:
+			fmt.Println("Number")
+		case miniblink.JSTYPE_STRING:
+			fmt.Println("String")
+			fmt.Println("返回值：", f.web.Webview.GlobalExec().ToTempString(val))
+		case miniblink.JSTYPE_BOOLEAN:
+			fmt.Println("Boolean")
+		case miniblink.JSTYPE_OBJECT:
+			fmt.Println("Object")
+		case miniblink.JSTYPE_FUNCTION:
+			fmt.Println("Function")
+		case miniblink.JSTYPE_UNDEFINED:
+			fmt.Println("Undefined")
+		case miniblink.JSTYPE_ARRAY:
+			fmt.Println("Array")
+		case miniblink.JSTYPE_NULL:
+			fmt.Println("Null")
+		default:
+			fmt.Println("未知")
+
+		}
+
 	}
 }
--- samples/miniblinkWebview/main.go
+++ samples/miniblinkWebview/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"github.com/ying32/govcl/vcl"
+	"github.com/ying32/govcl/vcl/exts/miniblink"
 )
 
 func main() {
 
+	miniblink.Init()
+	defer miniblink.Finalize()
 	vcl.Application.Initialize()
 	vcl.Application.SetMainFormOnTaskBar(true)
 	vcl.Application.CreateForm(mainFormBytes, &MainForm)
 	vcl.Application.CreateForm(hTMLFormBytes, &HTMLForm)
+	vcl.Application.CreateForm(jsFormBytes, &JsForm)
 	vcl.Application.Run()
 
 }
