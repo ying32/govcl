1f4b4ed7af8d1738d476e17f7b667749cf0183fa
ying32 <ying_33@live.cn>
2019-07-11 11:20:16 +0800

    Update memloaddll/main.go

--- samples/memloaddll/main.go
+++ samples/memloaddll/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/ying32/govcl/vcl"
+	"github.com/ying32/govcl/vcl/api"
 )
 
 func main() {
-
+	// Free
+	defer api.FeeMemoryDLL()
 	vcl.Application.Initialize()
 	vcl.Application.SetMainFormOnTaskBar(true)
 	vcl.Application.SetOnException(func(sender vcl.IObject, e *vcl.Exception) {
