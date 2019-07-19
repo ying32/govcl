9f77279e4e0492d89ed08f55077b2ed25016b545
ying32 <ying_33@live.cn>
2019-07-19 22:59:32 +0800

    添加一个windows下全局热键注册例程。
    Add a global hotkey registration example under Windows.

---
+++ samples/registerHotKey/Form1Impl.go
@@ -0,0 +1,54 @@
+// 在这里写你的事件
+
+package main
+
+import (
+	"github.com/ying32/govcl/vcl"
+	"github.com/ying32/govcl/vcl/rtl"
+	"github.com/ying32/govcl/vcl/types"
+	"github.com/ying32/govcl/vcl/types/keys"
+	"github.com/ying32/govcl/vcl/types/messages"
+	"github.com/ying32/govcl/vcl/win"
+)
+
+//::private::
+type TForm1Fields struct {
+	hotKeyId types.ATOM
+}
+
+func (f *TForm1) OnFormCreate(sender vcl.IObject) {
+	f.ScreenCenter()
+	f.hotKeyId = win.GlobalAddAtom("HotKeyId") - 0xC000
+	//rtl.ShortCutToText()
+	shift := rtl.Include(0, types.SsCtrl)
+	// 注册Ctrl+F1
+	// rtl.ShiftStateToWord(shift) 这个只是更容易理解，也可以使用 MOD_CONTROL | MOD_ALT 方法
+	if !win.RegisterHotKey(f.Handle(), int32(f.hotKeyId), rtl.ShiftStateToWord(shift), keys.VkF1) {
+		vcl.ShowMessage("注册热键失败。")
+	}
+
+}
+
+func (f *TForm1) OnFormDestroy(sender vcl.IObject) {
+	if f.hotKeyId > 0 {
+		win.UnregisterHotKey(f.Handle(), int32(f.hotKeyId))
+		win.GlobalDeleteAtom(f.hotKeyId)
+	}
+}
+
+func (f *TForm1) OnFormWndProc(msg *types.TMessage, handled *bool) {
+	/*
+	  TWMHotKey = record
+	    Msg: Cardinal;
+	    MsgFiller: TDWordFiller;
+	    HotKey: WPARAM;
+	    Unused: LPARAM;
+	    Result: LRESULT;
+	  end;
+	*/
+	if msg.Msg == messages.WM_HOTKEY {
+		if msg.WParam == types.WAPRAM(f.hotKeyId) {
+			vcl.ShowMessage("按下了Ctrl+F1")
+		}
+	}
+}
