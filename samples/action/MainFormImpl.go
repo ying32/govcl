d4dfd54964e4c87f04c2de4ce9e10fde7a901850
ying32 <ying_33@live.cn>
2020-02-06 21:58:21 +0800

    Update action sample

---
+++ samples/action/MainFormImpl.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"github.com/ying32/govcl/vcl"
+	"github.com/ying32/govcl/vcl/types"
+)
+
+type TMainForm struct {
+	*vcl.TForm
+	ImgList  *vcl.TImageList
+	ActList  *vcl.TActionList
+	Tlbar    *vcl.TToolBar
+	Tlbtn    *vcl.TToolButton
+	Stbar    *vcl.TStatusBar
+	Btn      *vcl.TButton
+	Chk      *vcl.TCheckBox
+	Act      *vcl.TAction
+	MainMenu *vcl.TMainMenu
+}
+
+var mainForm *TMainForm
+
+func (f *TMainForm) OnFormCreate(sender vcl.IObject) {
+
+	f.SetCaption("Hello")
+	f.SetPosition(types.PoScreenCenter)
+	f.EnabledMaximize(false)
+	f.SetWidth(300)
+	f.SetHeight(200)
+	// 全局设置提示
+	f.SetShowHint(true)
+
+	f.ImgList = vcl.NewImageList(f)
+	f.ImgList.AddIcon(vcl.Application.Icon())
+
+	f.ActList = vcl.NewActionList(f)
+	f.ActList.SetImages(f.ImgList)
+
+	// 顶部工具条
+	f.Tlbar = vcl.NewToolBar(f)
+	f.Tlbar.SetParent(f)
+	f.Tlbar.SetImages(f.ImgList)
+
+	f.Tlbtn = vcl.NewToolButton(f)
+	f.Tlbtn.SetParent(f.Tlbar)
+
+	// 底部状态条
+	f.Stbar = vcl.NewStatusBar(f)
+	f.Stbar.SetParent(f)
+	f.Stbar.SetAutoHint(true)
+	f.Stbar.SetSimplePanel(true)
+
+	f.Btn = vcl.NewButton(f)
+	f.Btn.SetParent(f)
+	f.Btn.SetLeft(80)
+	f.Btn.SetTop(f.Tlbar.Top() + f.Tlbar.Height() + 10)
+	f.Btn.SetImages(f.ImgList)
+
+	f.Chk = vcl.NewCheckBox(f)
+	f.Chk.SetParent(f)
+	f.Chk.SetCaption("action状态演示")
+	f.Chk.SetLeft(f.Btn.Left())
+	f.Chk.SetTop(f.Btn.Top() + f.Btn.Height() + 10)
+	f.Chk.SetChecked(true)
+
+	// action
+	f.Act = vcl.NewAction(f)
+	f.Act.SetCaption("action")
+	f.Act.SetImageIndex(0)
+	f.Act.SetHint("这是一个提示|长提示了")
+
+	// mainMenu
+	f.MainMenu = vcl.NewMainMenu(f)
+	f.MainMenu.SetImages(f.ImgList)
+
+	menu := vcl.NewMenuItem(f)
+	menu.SetCaption("菜单")
+	f.MainMenu.Items().Add(menu)
+	subMenu := vcl.NewMenuItem(f)
+	subMenu.SetAction(f.Act)
+	menu.Add(subMenu)
+
+	f.Btn.SetAction(f.Act)
+	f.Tlbtn.SetAction(f.Act)
+}
+
+func (f *TMainForm) OnActExecute(sender vcl.IObject) {
+	vcl.ShowMessage("点击了action")
+}
+
+func (f *TMainForm) OnActUpdate(sender vcl.IObject) {
+	vcl.ActionFromObj(sender).SetEnabled(f.Chk.Checked())
+}
--- samples/action/main.go
+++ samples/action/main.go
@@ -1,82 +1,10 @@
 package main
 
-import (
-	"github.com/ying32/govcl/vcl"
-	"github.com/ying32/govcl/vcl/types"
-)
+import "github.com/ying32/govcl/vcl"
 
 func main() {
-
 	vcl.Application.Initialize()
 	vcl.Application.SetMainFormOnTaskBar(true)
-
-	mainForm := vcl.Application.CreateForm()
-	mainForm.SetCaption("Hello")
-	mainForm.SetPosition(types.PoScreenCenter)
-	mainForm.EnabledMaximize(false)
-	mainForm.SetWidth(300)
-	mainForm.SetHeight(200)
-	// 全局设置提示
-	mainForm.SetShowHint(true)
-
-	imgList := vcl.NewImageList(mainForm)
-	imgList.AddIcon(vcl.Application.Icon())
-
-	actList := vcl.NewActionList(mainForm)
-	actList.SetImages(imgList)
-
-	// 顶部工具条
-	tlbar := vcl.NewToolBar(mainForm)
-	tlbar.SetParent(mainForm)
-	tlbar.SetImages(imgList)
-
-	tlbtn := vcl.NewToolButton(mainForm)
-	tlbtn.SetParent(tlbar)
-
-	// 底部状态条
-	stbar := vcl.NewStatusBar(mainForm)
-	stbar.SetParent(mainForm)
-	stbar.SetAutoHint(true)
-	stbar.SetSimplePanel(true)
-
-	btn := vcl.NewButton(mainForm)
-	btn.SetParent(mainForm)
-	btn.SetLeft(80)
-	btn.SetTop(tlbar.Top() + tlbar.Height() + 10)
-	btn.SetImages(imgList)
-
-	chk := vcl.NewCheckBox(mainForm)
-	chk.SetParent(mainForm)
-	chk.SetCaption("action状态演示")
-	chk.SetLeft(btn.Left())
-	chk.SetTop(btn.Top() + btn.Height() + 10)
-	chk.SetChecked(true)
-
-	// action
-	act := vcl.NewAction(mainForm)
-	act.SetCaption("action")
-	act.SetImageIndex(0)
-	act.SetHint("这是一个提示|长提示了")
-
-	act.SetOnExecute(func(vcl.IObject) {
-		vcl.ShowMessage("点击了action")
-	})
-	act.SetOnUpdate(func(sender vcl.IObject) {
-		vcl.ActionFromObj(sender).SetEnabled(chk.Checked())
-	})
-
-	mainMenu := vcl.NewMainMenu(mainForm)
-	mainMenu.SetImages(imgList)
-
-	menu := vcl.NewMenuItem(mainForm)
-	menu.SetCaption("菜单")
-	mainMenu.Items().Add(menu)
-	subMenu := vcl.NewMenuItem(mainForm)
-	subMenu.SetAction(act)
-	menu.Add(subMenu)
-
-	btn.SetAction(act)
-	tlbtn.SetAction(act)
-
+	vcl.Application.CreateForm(&mainForm, true)
 	vcl.Application.Run()
 }
