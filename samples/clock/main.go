445c5849883fcbef929dc7139798cc2310989668
ying32 <ying_33@live.cn>
2020-02-26 15:51:21 +0800

    Update clock sample

--- samples/clock/main.go
+++ samples/clock/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"github.com/ying32/govcl/vcl"
+    "github.com/ying32/govcl/vcl"
 )
 
 func main() {
 
-	vcl.Application.Initialize()
-	vcl.Application.SetMainFormOnTaskBar(true)
-	vcl.Application.CreateForm(form1Bytes, &Form1)
-	vcl.Application.Run()
+    vcl.Application.Initialize()
+    vcl.Application.SetMainFormOnTaskBar(true)
+    vcl.Application.CreateForm(&Form1)
+    vcl.Application.Run()
 
 }
+
