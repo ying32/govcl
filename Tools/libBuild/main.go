e18dd747661f3cc1278825effedaf2ace34ea1d0
ying32 <ying_33@live.cn>
2020-03-01 02:52:28 +0800

    Update Tools\libBuild\main.go

--- Tools/libBuild/main.go
+++ Tools/libBuild/main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"flag"
 	"log"
+	"runtime"
 )
 
 var (
 	// 编译库类型
-	libType = flag.String("lib", "", "要编译的目标库，可选为lcl、vcl")
-	libArch = flag.String("arch", "all", "编译的目标库arch，可选为x86、x64、all")
+	libType = flag.String("lib", "all", "要编译的目标库，可选为lcl、vcl、all，仅Windows下有效。")
+	libArch = flag.String("arch", "all", "编译的目标库arch，可选为x86、x64、all，仅Windows下有效")
 )
 
 func main() {
 	flag.Parse()
-	if *libType == "" {
-		log.Fatal("lib参数不能为空。")
+	switch runtime.GOOS {
+	case "windows":
+	case "linux":
+
+	case "darwin":
+	default:
+		log.Fatal("不支持：", runtime.GOOS)
 	}
 }
