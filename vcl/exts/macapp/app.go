32e9d56507025bcd25c9628ff2252ce1a48b9ba2
ying32 <ying_33@live.cn>
2020-03-03 00:32:04 +0800

    Update macapp package

--- vcl/exts/macapp/app.go
+++ vcl/exts/macapp/app.go
@@ -64,70 +64,68 @@ func init() {
 
 // 以一个Mac下的app形式运行
 // 调试下使用，正式发布的时候虽然可 以不用去掉，但也不咋好
-func runWithMacOSApp() error {
+func runWithMacOSApp() {
 
-	if len(os.Args) == 1 {
-		if strings.Contains(os.Args[0], "Contents/MacOS") {
-			return fmt.Errorf("_")
-		}
+	if strings.Contains(os.Args[0], ".app/Contents/MacOS") {
+		return
+	}
 
-		execName := os.Args[0][strings.LastIndex(os.Args[0], "/")+1:]
-		macContentsDir := os.Args[0] + ".app/Contents"
-		macOSDir := macContentsDir + "/MacOS"
-		macResources := macContentsDir + "/Resources"
-		execFile := macOSDir + "/" + execName
-		if !fileExists(macOSDir) {
-			if err := os.MkdirAll(macOSDir, 0755); err != nil {
-				return err
-			}
-		}
-
-		if !fileExists(macResources) {
-			os.MkdirAll(macResources, 0755)
-		}
-
-		resName := fmt.Sprintf("%s/%s.icns", macResources, execName)
-		if !fileExists(resName) {
-			ioutil.WriteFile(resName, macOSAppIcon, 0666)
-		}
-
-		liblclFileName := macOSDir + "/liblcl.dylib"
-		// 文件不存在，复制
-		if !fileExists(liblclFileName) {
-			libFileName := getdylib()
-			if fileExists(libFileName) {
-				copyFile(libFileName, liblclFileName)
-			}
-		} else {
-			// 文件存在，对比后更新
-			libFileName := getdylib()
-			if fileExists(libFileName) {
-				f1, _ := os.Stat(libFileName)
-				f2, _ := os.Stat(liblclFileName)
-				if f1.Size() != f2.Size() {
-					copyFile(libFileName, liblclFileName)
-				}
-			}
-		}
-
-		plistFileName := macContentsDir + "/Info.plist"
-		if !fileExists(plistFileName) {
-			ioutil.WriteFile(plistFileName, []byte(fmt.Sprintf(infoplist, execName, execName, execName, execName)), 0666)
-		}
-
-		pkgInfoFileName := macContentsDir + "/PkgInfo"
-		if !fileExists(pkgInfoFileName) {
-			ioutil.WriteFile(pkgInfoFileName, pkgInfo, 0666)
-		}
-
-		if copyFile(os.Args[0], execFile) == nil {
-			os.Chmod(execFile, 0755)
-			args := os.Args[:len(os.Args)-1]
-			cmd := exec.Command(execFile, args...)
-			cmd.Run()
-			os.Exit(0)
+	execName := os.Args[0][strings.LastIndex(os.Args[0], "/")+1:]
+	macContentsDir := os.Args[0] + ".app/Contents"
+	macOSDir := macContentsDir + "/MacOS"
+	macResources := macContentsDir + "/Resources"
+	execFile := macOSDir + "/" + execName
+	if !fileExists(macOSDir) {
+		if err := os.MkdirAll(macOSDir, 0755); err != nil {
+			return
 		}
 	}
 
-	return nil
+	if !fileExists(macResources) {
+		os.MkdirAll(macResources, 0755)
+	}
+
+	resName := fmt.Sprintf("%s/%s.icns", macResources, execName)
+	if !fileExists(resName) {
+		ioutil.WriteFile(resName, macOSAppIcon, 0666)
+	}
+
+	liblclFileName := macOSDir + "/liblcl.dylib"
+	// 文件不存在，复制
+	if !fileExists(liblclFileName) {
+		libFileName := getdylib()
+		if fileExists(libFileName) {
+			copyFile(libFileName, liblclFileName)
+		}
+	} else {
+		// 文件存在，对比后更新
+		libFileName := getdylib()
+		if fileExists(libFileName) {
+			f1, _ := os.Stat(libFileName)
+			f2, _ := os.Stat(liblclFileName)
+			if f1.Size() != f2.Size() {
+				copyFile(libFileName, liblclFileName)
+			}
+		}
+	}
+
+	plistFileName := macContentsDir + "/Info.plist"
+	if !fileExists(plistFileName) {
+		ioutil.WriteFile(plistFileName, []byte(fmt.Sprintf(infoplist, execName, execName, execName, execName)), 0666)
+	}
+
+	pkgInfoFileName := macContentsDir + "/PkgInfo"
+	if !fileExists(pkgInfoFileName) {
+		ioutil.WriteFile(pkgInfoFileName, pkgInfo, 0666)
+	}
+
+	if copyFile(os.Args[0], execFile) == nil {
+		os.Chmod(execFile, 0755)
+		args := os.Args[:len(os.Args)-1]
+		cmd := exec.Command(execFile, args...)
+		cmd.Run()
+		os.Exit(0)
+	}
+
+	return
 }
