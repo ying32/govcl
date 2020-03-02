5a96d8a77420882f252a7b2d9e9478614e84028f
ying32 <ying_33@live.cn>
2020-03-03 00:16:43 +0800

    update miniwebview/main.go

--- samples/miniwebview/main.go
+++ samples/miniwebview/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"github.com/ying32/govcl/vcl"
-	"github.com/ying32/govcl/vcl/exts/macapp"
+	_ "github.com/ying32/govcl/vcl/exts/macapp"
 	_ "github.com/ying32/govcl/vcl/exts/winappres"
 )
 
 func main() {
-	macapp.RunWithMacOSApp()
 	vcl.Application.Initialize()
 	vcl.Application.CreateForm(&MainForm)
 	vcl.Application.Run()
