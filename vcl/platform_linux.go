cbc4cf450161a6b564683d4b65433ada8d232aac
ying32 <ying_33@live.cn>
2020-03-17 12:03:17 +0800

    Update platform_linux.go

--- vcl/platform_linux.go
+++ vcl/platform_linux.go
@@ -9,17 +9,30 @@
 
 package vcl
 
-type GtkWindow uintptr
+type GdkWindow uintptr
 
 type GtkWidget uintptr
+
+type GtkFixed uintptr
+
+type XID uintptr
 
 // PGtkWidget
 //func HandleToPlatformHandle(h types.HWND) GtkWidget {
 //	return GtkWidget(h)
 //}
 
-func (f *TForm) PlatformWindow() GtkWindow {
-	//r, _, _ := NSWindow_FromForm.Call(f.instance)
-	//return GtkWindow(r)
-	return GtkWindow(0)
+func (f *TForm) PlatformWindow() GdkWindow {
+	//r, _, _ := GdkWindow_FromForm.Call(f.instance)
+	//return GdkWindow(r)
+	return GdkWindow(0)
+}
+
+//func (g GdkWindow) XID() XID {
+//	return 0
+//}
+
+// lz中首先是一个widget，然后上面用了一个fixedWidget来处理的。
+func (g GtkWidget) FixedWidget() GtkFixed {
+	return 0
 }
