0c092dfc96b6791aacf16f677fc1c76119000afb
ying32 <ying_33@live.cn>
2020-03-18 16:02:59 +0800

    Update nswindowTest example.

--- samples/nswindowTest/Form1Impl.go
+++ samples/nswindowTest/Form1Impl.go
@@ -5,24 +5,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/ying32/govcl/vcl/rtl"
+
 	"github.com/ying32/govcl/vcl"
 	"github.com/ying32/govcl/vcl/types"
 )
 
 //::private::
 type TForm1Fields struct {
+	isDown    bool
+	downPoint types.TPoint
 }
 
 func (f *TForm1) OnFormCreate(sender vcl.IObject) {
 	f.ScreenCenter()
-
-}
-
-func (f *TForm1) OnFormDestroy(sender vcl.IObject) {
-
-}
-
-func (f *TForm1) OnButton1Click(sender vcl.IObject) {
 	nsWin := f.PlatformWindow()
 
 	fmt.Println("nsWindow:", nsWin)
@@ -34,6 +30,21 @@ func (f *TForm1) OnButton1Click(sender vcl.IObject) {
 	nsWin.SetRepresentedURL(0)
 	//	// 视图的类型，应该是系统按钮什么的都算在主体视图中了
 	nsWin.SetStyleMask(nsWin.StyleMask() | types.NSWindowStyleMaskFullSizeContentView)
+
+	f.Panel1.SetOnAlignPosition(f.onCenterAlign)
+}
+
+func (f *TForm1) OnFormDestroy(sender vcl.IObject) {
+
+}
+
+func (f *TForm1) OnButton1Click(sender vcl.IObject) {
+
+}
+
+func (f *TForm1) onCenterAlign(sender *vcl.TWinControl, control *vcl.TControl, newLeft, newTop, newWidth, newHeight *int32, alignRect *types.TRect, alignInfo types.TAlignInfo) {
+	*newLeft = (alignRect.Width() - *newWidth) / 2
+	*newTop = (alignRect.Height() - *newHeight) / 2
 }
 
 /*
@@ -42,3 +53,25 @@ Lwin.SetTitlebarAppearsTransparent(True);
 Lwin.setStyleMask(Lwin.styleMask or NSWindowStyleMaskFullSizeContentView);
 Lwin.setRepresentedURL(nil);
 */
+
+func (f *TForm1) OnPanel1Click(sender vcl.IObject) {
+
+}
+
+func (f *TForm1) OnPanel1MouseDown(sender vcl.IObject, button types.TMouseButton, shift types.TShiftState, x, y int32) {
+	if button == types.MbLeft && !rtl.IsZoomed(f.Handle()) {
+		f.isDown = true
+		f.downPoint = types.Point(x, y)
+	}
+}
+
+func (f *TForm1) OnPanel1MouseMove(sender vcl.IObject, shift types.TShiftState, x, y int32) {
+	if f.isDown {
+		f.SetLeft(f.Left() + (x - f.downPoint.X))
+		f.SetTop(f.Top() + (y - f.downPoint.Y))
+	}
+}
+
+func (f *TForm1) OnPanel1MouseUp(sender vcl.IObject, button types.TMouseButton, shift types.TShiftState, x, y int32) {
+	f.isDown = false
+}
---
+++ samples/nswindowTest/ui/project1.lpr
@@ -0,0 +1,22 @@
+program project1;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, Unit1
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
