9b0bd8a3f98bd6f0aec30648b8032a2b042e5fb7
ying32 <ying_33@live.cn>
2020-03-20 23:11:07 +0800

    Add a `winappres/uac` package.

--- pkgs/winappres/uac/md.go
+++ pkgs/winappres/uac/md.go
@@ -6,12 +6,13 @@
 //
 //----------------------------------------
 
-package winappres
+package uac
 
 // 这是一个空的文件，用于导入，也只用于测试用
 // 生成的方式请前往wiki查看： https://gitee.com/ying32/govcl/wikis/pages?sort_id=410058&doc_id=102420
 
-// 使用方法为  import _ "github.com/ying32/govcl/pkgs/winappres
+// uac的
+// 使用方法为  import _ "github.com/ying32/govcl/pkgs/winappres/uac
 
 // 只有windows下有效，里面包含默认的一些东西
 
