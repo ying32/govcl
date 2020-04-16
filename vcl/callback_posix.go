e6f5a7e5a4868bbea0decfb678933f488bb26e9c
ying32 <ying_33@live.cn>
2020-04-17 03:00:15 +0800

    add cflags

--- vcl/callback_posix.go
+++ vcl/callback_posix.go
@@ -11,6 +11,8 @@
 
 package vcl
 
+// #cgo darwin CFLAGS: -mmacosx-version-min=10.5 -DMACOSX_DEPLOYMENT_TARGET=10.5
+//
 // extern void* doEventCallbackProc(void* f, void* args, long argcount);
 // static void* doGetEventCallbackAddr() {
 //    return &doEventCallbackProc;
