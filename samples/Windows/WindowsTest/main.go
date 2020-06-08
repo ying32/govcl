33295c02f9372b1b217bd798df18a87ebd7aa7e1
ying32 <ying_33@live.cn>
2020-06-08 15:01:02 +0800

    update Windows/WindowsTest example

--- samples/Windows/WindowsTest/main.go
+++ samples/Windows/WindowsTest/main.go
@@ -5,30 +5,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
+	"unsafe"
+
+	"github.com/ying32/govcl/vcl/win/errcode"
 
 	_ "github.com/ying32/govcl/pkgs/winappres"
 	"github.com/ying32/govcl/vcl/win"
 )
 
+var (
+	kernel32dll  = syscall.NewLazyDLL("kernel32.dll")
+	_CreateMutex = kernel32dll.NewProc("CreateMutexW")
+)
+
+// 不知道为什么GetLastError无法获取，只能重新申明下
+func CreateMutex(lpMutexAttributes *win.TSecurityAttributes, bInitialOwner bool, lpName string) (uintptr, uintptr, error) {
+	return _CreateMutex.Call(uintptr(unsafe.Pointer(lpMutexAttributes)), win.CBool(bInitialOwner), win.CStr(lpName))
+}
+
 func main() {
 
 	// 利用互斥来演示exe单一运行，当然不止这一种方法了
 	// GetLastError 无法获取错误，待研究原因所在。
-	/*
-		Mutex := win.CreateMutex(nil, true, "SingleRunTest")
-		defer win.ReleaseMutex(Mutex)
-		fmt.Println("Mutex:", Mutex)
-		fmt.Println("win.GetLastError:", win.GetLastError(), syscall.GetLastError())
-		if win.GetLastError() == win.ERROR_ALREADY_EXISTS {
-			win.MessageBox(0, "我已经在运行中啦！", "运行提示", win.MB_OK+win.MB_ICONINFORMATION)
-			os.Exit(1)
-		}
-
-		Mutex3 := win.CreateMutex(nil, true, "SingleRunTest")
-
-		fmt.Println("win.GetLastError:", win.GetLastError())
-		defer win.ReleaseMutex(Mutex3)
-	*/
+	Mutex, _, err := CreateMutex(nil, true, "SingleRunTest")
+	defer win.ReleaseMutex(Mutex)
+	fmt.Println("Mutex:", Mutex, err)
+	// if win.GetLastError() ==  {
+	// ??????????????????????????????????
+	if errNo, ok := err.(syscall.Errno); ok && errNo == errcode.ERROR_ALREADY_EXISTS {
+		win.MessageBox(0, "我已经在运行中啦！", "运行提示", win.MB_OK+win.MB_ICONINFORMATION)
+		os.Exit(1)
+	}
 
 	// Exe运行在Administrator权限下检测
 	fmt.Println("IsAdministrator:", win.IsAdministrator())
