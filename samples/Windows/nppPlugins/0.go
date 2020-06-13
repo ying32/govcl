66942f4f40ee53198a60fa9ce4f5cb6f8d7b8b5d
ying32 <ying_33@live.cn>
2020-06-13 21:01:56 +0800

    Fix nppPlughs example failed to load liblcl

---
+++ samples/Windows/nppPlugins/0.go
@@ -0,0 +1,11 @@
+//----------------------------------------
+//
+// Copyright © ying32. All Rights Reserved.
+//
+// Licensed under Apache License 2.0
+//
+//----------------------------------------
+
+package main
+
+import _ "github.com/ying32/govcl/samples/Windows/nppPlugins/alib"
--- samples/Windows/nppPlugins/main.go
+++ samples/Windows/nppPlugins/main.go
@@ -1,7 +1,5 @@
 package main
 
-import _ "github.com/ying32/govcl/samples/Windows/nppPlugins/alib"
-
 func main() {
 
 }
