626401a596be9dcf9540aef259a5251e2d84695a
不在乎y <ying_33@live.cn>
2022-09-29 13:58:58 +0800

    Added import API index comment

--- vcl/api/dllimports/funcs.go
+++ vcl/api/dllimports/funcs.go
@@ -9,34 +9,31 @@
 package dllimports
 
 import (
-	"sync"
+	"sync/atomic"
+	"unsafe"
 
 	"github.com/ying32/dylib"
 )
 
-var (
-	refs1 sync.Mutex
-	refs2 sync.Mutex
-)
+type importTable struct {
+	name string
+	proc *dylib.LazyProc
+}
 
 func GetImportFunc(uiLib *dylib.LazyDLL, index int) *dylib.LazyProc {
 	item := dllImports[index]
-	if item.proc == nil {
-		refs1.Lock()
-		defer refs1.Unlock()
+	if atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(&item.proc))) == nil {
 		item.proc = uiLib.NewProc(item.name)
-		dllImports[index] = item
+		atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&dllImports[index].proc)), unsafe.Pointer(item.proc))
 	}
 	return item.proc
 }
 
 func GetImportDefFunc(uiLib *dylib.LazyDLL, index int) *dylib.LazyProc {
 	item := dllImportDefs[index]
-	if item.proc == nil {
-		refs2.Lock()
-		defer refs2.Unlock()
+	if atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(&item.proc))) == nil {
 		item.proc = uiLib.NewProc(item.name)
-		dllImportDefs[index] = item
+		atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&dllImports[index].proc)), unsafe.Pointer(item.proc))
 	}
 	return item.proc
 }
