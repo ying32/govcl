b42bc1dbfbeb1e4a2366fb786c5922ad046f935f
不在乎y <ying_33@live.cn>
2022-10-01 03:49:40 +0800

    optimize

--- vcl/api/dllimports/dll_posix.go
+++ vcl/api/dllimports/dll_posix.go
@@ -80,46 +80,47 @@ func (h DLL) GetProcAddr(name string) ProcAddr {
 	return ProcAddr(proc)
 }
 
-func toPtr(p ProcAddr) unsafe.Pointer {
-	return unsafe.Pointer(p)
-}
-
-func toAPtr(p uintptr) unsafe.Pointer {
+func toPtr(p uintptr) unsafe.Pointer {
 	return unsafe.Pointer(p)
 }
 
 func (p ProcAddr) Call(args ...uintptr) (r1, r2 uintptr, err error) {
-	if p == 0 {
+	return SyscallN(uintptr(p), args...)
+}
+
+// SyscallN 调用一个函数，最大12个参数。
+func SyscallN(addr uintptr, args ...uintptr) (r1, r2 uintptr, err error) {
+	if addr == 0 {
 		return 0, 0, syscall.EINVAL
 	}
 	var ret C.uint64_t
 	switch len(args) {
 	case 0:
-		ret = C.Syscall0(toPtr(p))
+		ret = C.Syscall0(toPtr(addr))
 	case 1:
-		ret = C.Syscall1(toPtr(p), toAPtr(args[0]))
+		ret = C.Syscall1(toPtr(addr), toPtr(args[0]))
 	case 2:
-		ret = C.Syscall2(toPtr(p), toAPtr(args[0]), toAPtr(args[1]))
+		ret = C.Syscall2(toPtr(addr), toPtr(args[0]), toPtr(args[1]))
 	case 3:
-		ret = C.Syscall3(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]))
+		ret = C.Syscall3(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]))
 	case 4:
-		ret = C.Syscall4(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]), toAPtr(args[3]))
+		ret = C.Syscall4(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]), toPtr(args[3]))
 	case 5:
-		ret = C.Syscall5(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]), toAPtr(args[3]), toAPtr(args[4]))
+		ret = C.Syscall5(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]), toPtr(args[3]), toPtr(args[4]))
 	case 6:
-		ret = C.Syscall6(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]), toAPtr(args[3]), toAPtr(args[4]), toAPtr(args[5]))
+		ret = C.Syscall6(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]), toPtr(args[3]), toPtr(args[4]), toPtr(args[5]))
 	case 7:
-		ret = C.Syscall7(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]), toAPtr(args[3]), toAPtr(args[4]), toAPtr(args[5]), toAPtr(args[6]))
+		ret = C.Syscall7(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]), toPtr(args[3]), toPtr(args[4]), toPtr(args[5]), toPtr(args[6]))
 	case 8:
-		ret = C.Syscall8(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]), toAPtr(args[3]), toAPtr(args[4]), toAPtr(args[5]), toAPtr(args[6]), toAPtr(args[7]))
+		ret = C.Syscall8(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]), toPtr(args[3]), toPtr(args[4]), toPtr(args[5]), toPtr(args[6]), toPtr(args[7]))
 	case 9:
-		ret = C.Syscall9(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]), toAPtr(args[3]), toAPtr(args[4]), toAPtr(args[5]), toAPtr(args[6]), toAPtr(args[7]), toAPtr(args[8]))
+		ret = C.Syscall9(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]), toPtr(args[3]), toPtr(args[4]), toPtr(args[5]), toPtr(args[6]), toPtr(args[7]), toPtr(args[8]))
 	case 10:
-		ret = C.Syscall10(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]), toAPtr(args[3]), toAPtr(args[4]), toAPtr(args[5]), toAPtr(args[6]), toAPtr(args[7]), toAPtr(args[8]), toAPtr(args[9]))
+		ret = C.Syscall10(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]), toPtr(args[3]), toPtr(args[4]), toPtr(args[5]), toPtr(args[6]), toPtr(args[7]), toPtr(args[8]), toPtr(args[9]))
 	case 11:
-		ret = C.Syscall11(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]), toAPtr(args[3]), toAPtr(args[4]), toAPtr(args[5]), toAPtr(args[6]), toAPtr(args[7]), toAPtr(args[8]), toAPtr(args[9]), toAPtr(args[10]))
+		ret = C.Syscall11(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]), toPtr(args[3]), toPtr(args[4]), toPtr(args[5]), toPtr(args[6]), toPtr(args[7]), toPtr(args[8]), toPtr(args[9]), toPtr(args[10]))
 	case 12:
-		ret = C.Syscall12(toPtr(p), toAPtr(args[0]), toAPtr(args[1]), toAPtr(args[2]), toAPtr(args[3]), toAPtr(args[4]), toAPtr(args[5]), toAPtr(args[6]), toAPtr(args[7]), toAPtr(args[8]), toAPtr(args[9]), toAPtr(args[10]), toAPtr(args[11]))
+		ret = C.Syscall12(toPtr(addr), toPtr(args[0]), toPtr(args[1]), toPtr(args[2]), toPtr(args[3]), toPtr(args[4]), toPtr(args[5]), toPtr(args[6]), toPtr(args[7]), toPtr(args[8]), toPtr(args[9]), toPtr(args[10]), toPtr(args[11]))
 
 	default:
 		return 0, 0, errors.New("the number of invalid parameters")
--- vcl/api/dllimports/dll_windows.go
+++ vcl/api/dllimports/dll_windows.go
@@ -38,38 +38,42 @@ func (h DLL) GetProcAddr(name string) ProcAddr {
 }
 
 func (p ProcAddr) Call(args ...uintptr) (r1, r2 uintptr, err syscall.Errno) {
-	if p == 0 {
+	return SyscallN(uintptr(p), args...)
+}
+
+// SyscallN 调用一个函数，最大12个参数。
+func SyscallN(addr uintptr, args ...uintptr) (r1, r2 uintptr, err syscall.Errno) {
+	if addr == 0 {
 		return 0, 0, syscall.ERROR_PROC_NOT_FOUND
 	}
 	switch len(args) {
 	case 0:
-		return syscall.Syscall(uintptr(p), 0, 0, 0, 0)
+		return syscall.Syscall(addr, 0, 0, 0, 0)
 	case 1:
-		return syscall.Syscall(uintptr(p), 1, args[0], 0, 0)
+		return syscall.Syscall(addr, 1, args[0], 0, 0)
 	case 2:
-		return syscall.Syscall(uintptr(p), 2, args[0], args[1], 0)
+		return syscall.Syscall(addr, 2, args[0], args[1], 0)
 	case 3:
-		return syscall.Syscall(uintptr(p), 3, args[0], args[1], args[2])
+		return syscall.Syscall(addr, 3, args[0], args[1], args[2])
 	case 4:
-		return syscall.Syscall6(uintptr(p), 4, args[0], args[1], args[2], args[3], 0, 0)
+		return syscall.Syscall6(addr, 4, args[0], args[1], args[2], args[3], 0, 0)
 	case 5:
-		return syscall.Syscall6(uintptr(p), 5, args[0], args[1], args[2], args[3], args[4], 0)
+		return syscall.Syscall6(addr, 5, args[0], args[1], args[2], args[3], args[4], 0)
 	case 6:
-		return syscall.Syscall6(uintptr(p), 6, args[0], args[1], args[2], args[3], args[4], args[5])
+		return syscall.Syscall6(addr, 6, args[0], args[1], args[2], args[3], args[4], args[5])
 	case 7:
-		return syscall.Syscall9(uintptr(p), 7, args[0], args[1], args[2], args[3], args[4], args[5], args[6], 0, 0)
+		return syscall.Syscall9(addr, 7, args[0], args[1], args[2], args[3], args[4], args[5], args[6], 0, 0)
 	case 8:
-		return syscall.Syscall9(uintptr(p), 8, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], 0)
+		return syscall.Syscall9(addr, 8, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], 0)
 	case 9:
-		return syscall.Syscall9(uintptr(p), 9, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8])
+		return syscall.Syscall9(addr, 9, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8])
 	case 10:
-		return syscall.Syscall12(uintptr(p), 10, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], 0, 0)
+		return syscall.Syscall12(addr, 10, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], 0, 0)
 	case 11:
-		return syscall.Syscall12(uintptr(p), 11, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], 0)
+		return syscall.Syscall12(addr, 11, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], 0)
 	case 12:
-		return syscall.Syscall12(uintptr(p), 12, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11])
+		return syscall.Syscall12(addr, 12, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11])
 	default:
 		return 0, 0, 87 //ERROR_INVALID_PARAMETER = 87
 	}
-
 }
